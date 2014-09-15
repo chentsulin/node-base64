58152404fe6377b836680359b25695fe80a99981
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-09-15 23:09:18 +0800

    bug fix and error throw

--- node-base64.js
+++ node-base64.js
@@ -1,24 +1,33 @@
 
-
 module.exports.encode = function(str) {
+
+    if (typeof str  !== 'string' || str  === '') {
+
+        throw new TypeError('parameter is not a vaild string');
+    }
 
     return new Buffer(str).toString('base64');
 }
 
 
-module.exports.decode = function(str) {
+module.exports.decode = function(hash) {
 
-    return new Buffer(afterBase64, 'base64').toString();
+    if (typeof hash !== 'string' || hash === '') {
+
+        throw new TypeError('parameter is not a vaild string');
+    }
+
+    return new Buffer(hash, 'base64').toString();
 }
 
 module.exports.compare = function(str, hash) {
 
     if (typeof str  !== 'string' || str  === '' ||
         typeof hash !== 'string' || hash === '') {
-    
+
         return false;
     }
-    
-    return new Buffer(str).toString('base64') === hash;
-    
+
+    return (new Buffer(str).toString('base64')) === hash;
+
 }
